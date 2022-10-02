2d51a40d3b364259bcb0e68fb900d2b66b989c96
S-Delowar <sayed.buet97@gmail.com>
2022-10-02 20:29:24 +0600

    binary tree added

---
+++ binaryTree.py
@@ -0,0 +1,43 @@
+from queue import Queue
+from binaryTreePrinter import BinaryTreePrinter
+
+
+class TreeNode:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.val = value
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        if self.root is None:
+            self.root = TreeNode(val)
+        else:
+            nodes = Queue()
+            nodes.enqueue(self.root)
+
+            while True:
+                checking_node = nodes.deque()
+                if checking_node.left is None:
+                    checking_node.left = TreeNode(val)
+                    return
+                elif checking_node.right is None:
+                    checking_node.right = TreeNode(val)
+                    return
+                else:
+                    nodes.enqueue(checking_node.left)
+                    nodes.enqueue(checking_node.right)
+
+    def __str__(self):
+        tree_printer = BinaryTreePrinter()
+        return tree_printer.get_tree_string(self.root)
+
+my_tree = BinaryTree()
+
+for c in ['a','b','c','d','e','f','g','h']:
+    my_tree.insert(c)
+    print(my_tree)
